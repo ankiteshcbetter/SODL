0c152698f1aa0b79b09e4c3d1161aad1b7fb656c
ankitesh <ankitesh.arora@cbetter.in>
2018-08-29 10:31:56 +0530

    Added new section and jquery scroll

--- js/jquery.js
+++ js/jquery.js
@@ -2,4 +2,30 @@ $('.collapse').collapse();
 
 
 
+// $('.btn-link').click(function(){
+//     $('.card-header')
+// })
 
+
+$('.page-scroll').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href');
+    console.log(id);
+    $('html, body').animate({scrollTop: $(id).offset().top}, 1000);
+
+
+});
+
+$(window).scroll(function () {
+
+    var scrollPos = jQuery(this).scrollTop();
+    var digital =
+
+    if (scrollPos > 300) {
+        jQuery('nav.navbar').addClass('sticky__color');
+    } else {
+        jQuery('nav.navbar').removeClass('sticky__color');
+    }
+
+
+});
